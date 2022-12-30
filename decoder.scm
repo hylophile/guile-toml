a6474dd0498e1f291da19e562bde648e62573495
hylo <hylo@posteo.de>
2022-12-30 17:53:13 +0100

    docs: update readme

--- decoder.scm
+++ decoder.scm
@@ -1,10 +1,8 @@
 #!/usr/bin/env -S guile -s
 !#
-(use-modules (json) (toml) (ice-9 match) (ice-9 pretty-print) (ice-9 textual-ports))
+(use-modules (toml) (ice-9 pretty-print) (ice-9 textual-ports))
 
 (define str (get-string-all (current-input-port)))
 (define scm (toml->scm str))
 
 (pretty-print scm)
-
-;; (define json (scm->json scm #:pretty #t))
