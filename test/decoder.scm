50828e2ee595557a4435469e1dc8b43a519b38e1
hylo <hylo@posteo.de>
2022-12-30 18:53:44 +0100

    nit: formatting of decoder

--- test/decoder.scm
+++ test/decoder.scm
@@ -1,6 +1,9 @@
 #!/usr/bin/env -S guile -s
 !#
-(use-modules (toml) (ice-9 pretty-print) (ice-9 textual-ports))
+(use-modules
+ (toml)
+ (ice-9 pretty-print)
+ (ice-9 textual-ports))
 
 (define str (get-string-all (current-input-port)))
 (define scm (toml->scm str))
